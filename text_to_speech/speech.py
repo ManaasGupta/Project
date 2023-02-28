52244c6aa49a1a9751c490177ef9c413fc927c03
ManaasGupta <manasgpt3@gmail.com>
2023-02-28 13:04:20 +0530

    Updated Files

--- text_to_speech/speech.py
+++ text_to_speech/speech.py
@@ -3,19 +3,20 @@ from playsound import playsound
 import os
 from s2t import s2t
 import time 
+
+
 start_timer=time.time()
 start_timer_cpu=time.process_time() 
 
-def speech(text:str,file_name:str):
+def speech(text:str):
     tts = gtts.gTTS(text)
-    file_name=file_name+'.mp3'
+    file_name='text.mp3'
     tts.save(file_name)
     playsound(file_name)
     os.remove(file_name)
 
 text=str(s2t())
-file_name=str('test')
-speech(text,file_name)
+speech(text)
 
 end_timer=time.time()
 end_timer_cpu=time.process_time() 
