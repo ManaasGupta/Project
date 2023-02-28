bf31e0b522c34970df1090d8bcb77c2ed2ae3258
ManaasGupta <manasgpt3@gmail.com>
2023-02-28 12:50:23 +0530

    Updataed Files

---
+++ text_to_speech/s2t.py
@@ -0,0 +1,24 @@
+import speech_recognition as sr
+
+def s2t():
+# Initialize recognizer
+    r = sr.Recognizer()
+
+    # Use microphone as source
+    with sr.Microphone() as source:
+        print("Please wait. Calibrating microphone...")  
+       # listen for 5 seconds and create the ambient noise energy level  
+        r.adjust_for_ambient_noise(source) 
+        print("Say something...")
+        audio = r.listen(source)
+
+    # Convert speech to text
+    try:
+        text = r.recognize_google(audio)
+        print("You said: {}".format(text))
+    except sr.UnknownValueError:
+        print("Oops! Unable to recognize speech.")
+    except sr.RequestError as e:
+        print("Oops! Could not request results from Google Speech Recognition service; {0}".format(e))
+    
+    return text 
--- text_to_speech/speech.py
+++ text_to_speech/speech.py
@@ -1,6 +1,10 @@
 import gtts
 from playsound import playsound
 import os
+from s2t import s2t
+import time 
+start_timer=time.time()
+start_timer_cpu=time.process_time() 
 
 def speech(text:str,file_name:str):
     tts = gtts.gTTS(text)
@@ -9,6 +13,16 @@ def speech(text:str,file_name:str):
     playsound(file_name)
     os.remove(file_name)
 
-text=str(input('Enter what you meant to say: '))
-file_name=str(input('Enter the path where you mean to save file: '))
+text=str(s2t())
+file_name=str('test')
 speech(text,file_name)
+
+end_timer=time.time()
+end_timer_cpu=time.process_time() 
+
+total_time=end_timer - start_timer
+total_time_cpu=end_timer_cpu - start_timer_cpu
+
+
+print(f'Total time for execution of program {total_time}')
+print(f'Total process time for cpu was {total_time_cpu}')
